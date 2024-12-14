0c7174a7fae276c03e38e627e65644a2de597485
harshit118garg <harshit118garg@gmail.com>
2024-12-14 13:33:19 +0530

    accordion component

--- src/UI/accordion/index.tsx
+++ src/UI/accordion/index.tsx
@@ -3,7 +3,7 @@ import AccordionItem from "./components/AccordionItem";
 
 export default function Accordion() {
   return (
-    <div>
+    <>
       <h2>Accordion</h2>
       <div className="wrapper bg-green-300 p-4 rounded-xl">
         <div className="accordion-items p-2 flex flex-col gap-4">
@@ -16,6 +16,6 @@ export default function Accordion() {
           })}
         </div>
       </div>
-    </div>
+    </>
   );
 }
